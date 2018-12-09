377f0af67eb4630bb9f527e13bcec0a38bcf3a3c
Wellington Felix <wfmf@cin.ufpe.br>
2018-12-09 04:46:19 -0300

    Fix pre-push script

--- satellite/pre-push.sh
+++ satellite/pre-push.sh
@@ -1,2 +1,3 @@
-docker build -t wellfelix/satellite ./satellite
+DOCKERFILE_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)
+docker build -t wellfelix/satellite $DOCKERFILE_PATH
 docker push wellfelix/satellite
