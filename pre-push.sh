751968b4ffba356e5c01eac5c38f957210d948cd
Wellington Felix <wfmf@cin.ufpe.br>
2018-12-09 23:57:03 -0300

    Fix pre-push script

--- pre-push.sh
+++ pre-push.sh
@@ -1,7 +1,6 @@
+#!/bin/bash
 PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)
-
-docker build -t wellfelix/satellite $PATH/satellite
-docker push wellfelix/satellite
-
-docker build -t wellfelix/simple-haproxy $PATH/haproxy
-docker push wellfelix/simple-haproxy
+/usr/local/bin/docker build -t wellfelix/satellite $PATH/satellite
+/usr/local/bin/docker push wellfelix/satellite
+/usr/local/bin/docker build -t wellfelix/simple-haproxy $PATH/haproxy
+/usr/local/bin/docker push wellfelix/simple-haproxy
