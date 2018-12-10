0be57b88e84582fa5543c12b64aec1aea9cc8aa9
Wellington Felix <wfmf@cin.ufpe.br>
2018-12-10 00:16:08 -0300

    Fix httpbin server name

--- pre-push.sh
+++ pre-push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)
-/usr/local/bin/docker build -t wellfelix/satellite $PATH/satellite
-/usr/local/bin/docker push wellfelix/satellite
-/usr/local/bin/docker build -t wellfelix/simple-haproxy $PATH/haproxy
-/usr/local/bin/docker push wellfelix/simple-haproxy
+SCRIPT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)
+docker build -t wellfelix/satellite $SCRIPT_PATH/satellite
+docker push wellfelix/satellite
+docker build -t wellfelix/simple-haproxy $SCRIPT_PATH/haproxy
+docker push wellfelix/simple-haproxy
